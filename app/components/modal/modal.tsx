ddc157b44114acc0a987c1cfc5f414423d29e2ed
JesseKuntz <jessekuntz96@gmail.com>
2023-11-24 12:37:42 -0500

    fix: modal on mobile

--- app/components/modal/modal.tsx
+++ app/components/modal/modal.tsx
@@ -9,7 +9,7 @@ export const Modal: React.FC<Props> = ({ trigger, children }) => (
     <RadixDialog.Trigger>{trigger}</RadixDialog.Trigger>
     <RadixDialog.Portal>
       <RadixDialog.Overlay className="fixed z-10 bg-black/50 top-0 bottom-0 left-0 right-0 grid place-items-center overflow-y-auto" />
-      <RadixDialog.Content className="fixed z-20 rounded bg-slate-800 border-2 border-slate-700 p-6 z-modal max-h-[96vh] overflow-y-auto w-11/12 md:w-full top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] pointer-events-auto md:max-w-2xl">
+      <RadixDialog.Content className="fixed z-20 rounded bg-slate-800 border-2 border-slate-700 p-6 md:max-h-[96vh] max-h-full overflow-y-auto w-full md:max-w-2xl top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] pointer-events-auto">
         {children}
         <RadixDialog.Close className="absolute top-2 right-2">
           <Button palette="tertiary" icon as="div">
