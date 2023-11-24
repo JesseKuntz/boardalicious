7660c0c575c18287c0194a9ab995a8866b8014a1
JesseKuntz <jessekuntz96@gmail.com>
2023-11-24 11:50:09 -0500

    fix: tooltip hidden on modal open

--- app/components/tooltip/tooltip.tsx
+++ app/components/tooltip/tooltip.tsx
@@ -1,20 +1,32 @@
 import * as RadixTooltip from "@radix-ui/react-tooltip";
+import { useToggle } from "usehooks-ts";
 
 type Props = {
   trigger: React.ReactNode;
   children: React.ReactNode;
 };
 
-export const Tooltip: React.FC<Props> = ({ trigger, children }) => (
-  <RadixTooltip.Provider>
-    <RadixTooltip.Root delayDuration={0}>
-      <RadixTooltip.Trigger>{trigger}</RadixTooltip.Trigger>
-      <RadixTooltip.Portal>
-        <RadixTooltip.Content className="bg-black p-4 rounded max-w-sm z-50">
-          <RadixTooltip.Arrow width={20} height={10} />
-          {children}
-        </RadixTooltip.Content>
-      </RadixTooltip.Portal>
-    </RadixTooltip.Root>
-  </RadixTooltip.Provider>
-);
+export const Tooltip: React.FC<Props> = ({ trigger, children }) => {
+  const [openTooltip, toggleTooltip, setTooltip] = useToggle();
+
+  return (
+    <RadixTooltip.Provider>
+      <RadixTooltip.Root open={openTooltip}>
+        <RadixTooltip.Trigger asChild>
+          <span
+            onMouseEnter={() => setTooltip(true)}
+            onMouseLeave={() => setTooltip(false)}
+          >
+            {trigger}
+          </span>
+        </RadixTooltip.Trigger>
+        <RadixTooltip.Portal>
+          <RadixTooltip.Content className="bg-black p-4 rounded max-w-sm z-50">
+            <RadixTooltip.Arrow width={20} height={10} />
+            {children}
+          </RadixTooltip.Content>
+        </RadixTooltip.Portal>
+      </RadixTooltip.Root>
+    </RadixTooltip.Provider>
+  );
+};
